0c77f9222d84aa44d0d9d4b27c71af875fd2f5de
Udaysri20 <142375587+Udaysri20@users.noreply.github.com>
2025-05-10 18:08:27 +0530

    Updated

--- Student.java
+++ Student.java
@@ -1,8 +1,8 @@
 public class Student {
     public static void main(String[] args) {
-        int rollno = 1596;
-        String sname = "KRISHNA";
-        String course = "PYTHON"; 
+        int rollno = 3020;
+        String sname = "Kumar";
+        String course = "Devops"; 
         System.out.println("LEARN HERE AND LEAD ANYWHERE!!!");
         System.out.println("My Name is: " + sname);
         System.out.println("My Course is: " + course);
